98299e5578a7f680a1b8ca2583f492eabc470aa3
jia_yin <jia_yin@celfras.com>
2020-12-02 17:12:24 +0800

    add isc commands

--- prx.py
+++ prx.py
@@ -1,3 +1,4 @@
+import time
 from dongle import Dongle
 
 PRX_SLAVE_ADDR = 0x24
@@ -17,17 +18,93 @@ class Prx():
         self.dgl.close()
 
     def isc_enter(self):
-        cmd = [0xFB, 0x4A, 0x00, 0x65, 0x00, 0x00, 0x00, 0x00]
+        cmd = [ 0xFB, 
+                0x4A, 
+                0x00, 
+                0x65, 
+                0x00, 
+                0x00, 
+                0x00, 
+                0x00 ]
         self.dgl.write(PRX_SLAVE_ADDR, cmd)
 
     def isc_exit(self):
-        cmd = [0xFB, 0x4A, 0x00, 0x66, 0x00, 0x00, 0x00, 0x00]
+        cmd = [ 0xFB, 
+                0x4A, 
+                0x00, 
+                0x66, 
+                0x00, 
+                0x00, 
+                0x00, 
+                0x00 ]
         self.dgl.write(PRX_SLAVE_ADDR, cmd)
 
     def getid(self):
-        cmd = [0xFB, 0x4A, 0x50, 0xC2, 0x00, 0x00, 0x00, 0x00]
+        cmd = [ 0xFB, 
+                0x4A, 
+                0x50, 
+                0xC2, 
+                0x00, 
+                0x00, 
+                0x00, 
+                0x00 ]
         self.dgl.write(PRX_SLAVE_ADDR, cmd)
         data = self.dgl.read(PRX_SLAVE_ADDR, 2)
         id = (data[0] << 8) | data[1]
         print("CHIP ID: " + hex(id))
         return id
+
+    def writesram(self, addr, data):
+        cmd = [ 0xFB, 
+                0x4A, 
+                0xE0, 
+                0xA0, 
+                (addr >> 24) & 0xFF,
+                (addr >> 16) & 0xFF,
+                (addr >> 8)  & 0xFF,
+                (addr >> 0)  & 0xFF,
+                data ]
+        self.dgl.write(PRX_SLAVE_ADDR, cmd)
+
+    def wait(self, timeout):
+        cmd = [ 0xFB, 
+                0x4A, 
+                0x36, 
+                0xC2, 
+                0x00, 
+                0x00, 
+                0x00, 
+                0x00 ]
+        t = 0
+        while (t < timeout):
+            t += 0.25
+            time.sleep(0.25)
+            self.dgl.write(PRX_SLAVE_ADDR, cmd)
+            buf = self.dgl.read(PRX_SLAVE_ADDR, 1)
+            if buf[0] == 0xAD:
+                return True
+            return False
+
+    def readpage(self, addr, size):
+        cmd = [ 0xFB, 
+                0x4A, 
+                0x00, 
+                0xC2, 
+                (addr >> 24) & 0xFF,
+                (addr >> 16) & 0xFF,
+                (addr >> 8)  & 0xFF,
+                (addr >> 0)  & 0xFF ]
+        self.dgl.write(PRX_SLAVE_ADDR, cmd)
+        return self.dgl.read(PRX_SLAVE_ADDR, size)
+
+    def writepage(self, addr, data, size):
+        cmd = [ 0xFB, 
+                0x4A, 
+                0x00, 
+                0xA5,
+                (addr >> 24) & 0xFF,
+                (addr >> 16) & 0xFF,
+                (addr >> 8)  & 0xFF,
+                (addr >> 0)  & 0xFF ]
+        self.dgl.write(PRX_SLAVE_ADDR, cmd + data)
+        return self.wait(3)
