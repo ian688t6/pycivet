dcec42257dec3041399f9ac7e184a66271f97639
jia_yin <jia_yin@celfras.com>
2020-12-03 10:43:45 +0800

    add KeyboardInterrupt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import logging
+import os
+import sys
 from prx import Prx
 
 if __name__ == "__main__":
@@ -16,4 +18,10 @@ if __name__ == "__main__":
                 logging.info(logline)
         except UnicodeDecodeError:
             pass
+        except BaseException as e:
+            if isinstance(e, KeyboardInterrupt):
+                logging.info('pycivet exit')
+                rx.disconnect()
+                sys.exit(0)
     rx.disconnect()
+
