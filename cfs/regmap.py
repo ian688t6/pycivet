06380b791a19dcf68fc9806535e3ef3ba5662c60
ian688t6 <gagovski@163.com>
2020-12-07 20:31:01 +0800

    add ui menubutton and implete import and open function

--- cfs/regmap.py
+++ cfs/regmap.py
@@ -5,25 +5,25 @@ class RegMap():
     def __init__(self):
         pass
 
-    def __init_data(self, data):
+    def _init_data(self, data):
         sregex = re.compile(r'(\d+.\d+.\d+)\sAddr\s(0x[0-9a-fA-F]{8})\s(.+)')
         mo = sregex.search(data)
         regdata = {}
-        regdata['addr'] = int(mo.group(2), 16)
+        regdata['addr'] = mo.group(2)
         regdata['description'] = mo.group(3)
-        regdata['value'] = 0
+        regdata['value'] = '0'
         return regdata
 
     def load(self, worksheet):
-        self.__regmaps = load_workbook(worksheet, data_only=True)
+        self._regmaps = load_workbook(worksheet, data_only=True)
 
     def get_regblocks(self):
-        return self.__regmaps.sheetnames[2:]
+        return self._regmaps.sheetnames[2:]
 
     def get_regmap(self, block):
-        regmap = self.__regmaps[block]
+        regmap = self._regmaps[block]
         regmaplist = [ 
-        self.__init_data(d[0]) 
+        self._init_data(d[0]) 
         for d in regmap.iter_rows(min_row=1, max_row=regmap.max_row, min_col=3, max_col=3, values_only=True) 
         if None not in d and 'Addr' in d[0] ]
         return regmaplist 
